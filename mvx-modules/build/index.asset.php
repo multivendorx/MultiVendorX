ff0db3f0830525c47399028e526b12322dc9ffed
Souvik2799s <souvik@dualcube.com>
2022-05-31 00:03:55 +0530

    update

--- mvx-modules/build/index.asset.php
+++ mvx-modules/build/index.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-polyfill'), 'version' => 'fd9939b78d7b2daf4dfcb4a6a8088178');
-=======
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-polyfill'), 'version' => '80fa00a9e2b436fc5428a747676a5571');
->>>>>>> upstream/main
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-polyfill'), 'version' => '05f9fe934d58e83b9b34f4804df08f59');
