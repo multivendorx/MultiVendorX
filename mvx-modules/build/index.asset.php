9e5fe3e41033c84de04686d85424cd7a13d57b4b
Souvik2799s <souvik@dualcube.com>
2022-06-07 17:17:42 +0530

    Css update

--- mvx-modules/build/index.asset.php
+++ mvx-modules/build/index.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-polyfill'), 'version' => 'c7b1a2209946a4989146cd4671a43eb9');
-=======
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-polyfill'), 'version' => '80a35d9b24829e3a02bdc959289dc15a');
->>>>>>> upstream/main
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-polyfill'), 'version' => '3ed7e003eea40bbcef2de248f343a726');
