24bf43d36f72ce2f1168bf7f9aacbae75a78a571
moumitahalder <moumita.h@dualcube.com>
2022-07-06 02:11:22 +0530

    Update mvx-deprecated-funtions.php

--- includes/mvx-deprecated-funtions.php
+++ includes/mvx-deprecated-funtions.php
@@ -29,3 +29,69 @@ function wcmp_setup_store() {
     wc_deprecated_function( 'wcmp_setup_store', '4.0.0', 'mvx_setup_store');
     return mvx_setup_store();
 }
+
+
+function wcmp_setup_commission( ) {
+    wc_deprecated_function( 'wcmp_setup_commission )', '4.0.0', 'mvx_setup_commission');
+    return mvx_setup_commission();
+}
+
+function wcmp_setup_payments() {
+    wc_deprecated_function( 'wcmp_setup_payments', '4.0.0', 'mvx_setup_payments');
+    return mvx_setup_payments();
+}
+function wcmp_setup_capability() {
+    wc_deprecated_function( 'wcmp_setup_capability )', '4.0.0', 'mvx_setup_capability');
+    return mvx_setup_capability();
+}
+function wcmp_setup_ready() {
+    wc_deprecated_function( 'wcmp_setup_ready', '4.0.0', 'mvx_setup_ready');
+    return mvx_setup_ready();
+}
+function wcmp_setup_store_save() {
+    wc_deprecated_function( 'wcmp_setup_store_save', '4.0.0', 'mvx_setup_store_save');
+    return mvx_setup_store_save();
+}
+function wcmp_migration_introduction( ) {
+    wc_deprecated_function( 'wcmp_migration_introduction )', '4.0.0', 'mvx_migration_introduction');
+    return mvx_migration_introduction();
+}
+
+function wcmp_setup_commission_save() {
+    wc_deprecated_function( 'wcmp_setup_commission_save', '4.0.0', 'mvx_setup_commission_save');
+    return mvx_setup_commission_save();
+}
+function wcmp_setup_payments_save() {
+    wc_deprecated_function( 'wcmp_setup_payments_save )', '4.0.0', 'mvx_setup_payments_save');
+    return mvx_setup_payments_save();
+}
+function wcmp_setup_capability_save() {
+    wc_deprecated_function( 'wcmp_setup_capability_save', '4.0.0', 'mvx_setup_capability_save');
+    return mvx_setup_capability_save();
+}
+function wcmp_migration_store_process() {
+    wc_deprecated_function( 'wcmp_migration_store_process', '4.0.0', 'mvx_migration_store_process');
+    return mvx_migration_store_process();
+}
+
+
+function wcmp_approve_dismiss_pending_transaction($request){
+    wc_deprecated_function( 'wcmp_approve_dismiss_pending_transaction', '4.0.0', 'mvx_approve_dismiss_pending_transaction');
+    return mvx_approve_dismiss_pending_transaction($request);
+}
+function wcmp_modules_callback() {
+    wc_deprecated_function( 'wcmp_modules_callback', '4.0.0', 'mvx_modules_callback');
+    return mvx_modules_callback();
+}
+function wcmp_rest_routes_react_module() {
+    wc_deprecated_function( 'wcmp_rest_routes_react_module )', '4.0.0', 'mvx_rest_routes_react_module');
+    return mvx_rest_routes_react_module();
+}
+function wcmp_list_of_all_tabs() {
+    wc_deprecated_function( 'wcmp_list_of_all_tabs', '4.0.0', 'mvx_list_of_all_tabs');
+    return mvx_list_of_all_tabs();
+}
+function wcmp_list_of_bulk_change_status_question($request) {
+    wc_deprecated_function( 'wcmp_list_of_bulk_change_status_question', '4.0.0', 'mvx_list_of_bulk_change_status_question');
+    return mvx_list_of_bulk_change_status_question($request)
+}
