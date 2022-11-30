2fe2f9592842499c8e1adfea6285681a2ab99bcd
cool-raju <rajsekhar@dualcube.com>
2022-11-30 11:46:14 +0530

    update

--- mvx-modules/build/index.asset.php
+++ mvx-modules/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-polyfill'), 'version' => '6f5c70a6a8808a89485d1095abd1a320');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-polyfill'), 'version' => '6f044a1493495e0ab3902167311b4dc6');
