b41f6b1d6d37fabb6f0ff5903b78ffb83efc7f5d
cool-raju <rajsekhar@dualcube.com>
2023-03-07 18:28:13 +0530

    plugin updated with updated node and npm version

--- mvx-modules/build/index.asset.php
+++ mvx-modules/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-polyfill'), 'version' => '57820ebc7b4b47fe9676');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-polyfill'), 'version' => '11504a61234e1e9a947b');
