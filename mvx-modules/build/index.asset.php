b05137396679cc93ba114168cee95602fdd01d73
kundandualcube <113882686+kundandualcube@users.noreply.github.com>
2023-06-02 11:06:09 +0530

    #399 fixed & wait for test

--- mvx-modules/build/index.asset.php
+++ mvx-modules/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-polyfill'), 'version' => '738e2c1828b12e067382');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '8e97cb6fc9a0503a6fe5');
