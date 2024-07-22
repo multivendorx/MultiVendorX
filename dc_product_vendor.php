36596a600b54176de49bb1f3757b4faf38caf3e6
Promita Ghosh <118420070+user-promi@users.noreply.github.com>
2024-07-22 17:12:22 +0530

    change log added

--- dc_product_vendor.php
+++ dc_product_vendor.php
@@ -4,12 +4,12 @@
  * Plugin URI: https://multivendorx.com/
  * Description: A Free Extension That Transforms Your WooCommerce Site into a Marketplace.
  * Author: MultiVendorX
- * Version: 4.1.15
+ * Version: 4.1.16
  * Author URI: https://multivendorx.com/
  * Requires at least: 5.4
  * Tested up to: 6.5.5
  * WC requires at least: 8.2.2
- * WC tested up to: 9.0.2
+ * WC tested up to: 9.1.2
  *
  * Text Domain: multivendorx
  * Requires Plugins: woocommerce
