108e1eb839118c8936a3d6f1b4288bb88f806711
Promita Ghosh <118420070+user-promi@users.noreply.github.com>
2024-08-30 11:56:45 +0530

    stripe library updated

--- lib/Stripe/lib/Service/Treasury/FinancialAccountService.php
+++ lib/Stripe/lib/Service/Treasury/FinancialAccountService.php
@@ -4,13 +4,17 @@
 
 namespace Stripe\Service\Treasury;
 
+/**
+ * @phpstan-import-type RequestOptionsArray from \Stripe\Util\RequestOptions
+ * @psalm-import-type RequestOptionsArray from \Stripe\Util\RequestOptions
+ */
 class FinancialAccountService extends \Stripe\Service\AbstractService
 {
     /**
      * Returns a list of FinancialAccounts.
      *
      * @param null|array $params
-     * @param null|array|\Stripe\Util\RequestOptions $opts
+     * @param null|RequestOptionsArray|\Stripe\Util\RequestOptions $opts
      *
      * @throws \Stripe\Exception\ApiErrorException if the request fails
      *
@@ -26,7 +30,7 @@ class FinancialAccountService extends \Stripe\Service\AbstractService
      * one FinancialAccount.
      *
      * @param null|array $params
-     * @param null|array|\Stripe\Util\RequestOptions $opts
+     * @param null|RequestOptionsArray|\Stripe\Util\RequestOptions $opts
      *
      * @throws \Stripe\Exception\ApiErrorException if the request fails
      *
@@ -42,7 +46,7 @@ class FinancialAccountService extends \Stripe\Service\AbstractService
      *
      * @param string $id
      * @param null|array $params
-     * @param null|array|\Stripe\Util\RequestOptions $opts
+     * @param null|RequestOptionsArray|\Stripe\Util\RequestOptions $opts
      *
      * @throws \Stripe\Exception\ApiErrorException if the request fails
      *
@@ -58,11 +62,11 @@ class FinancialAccountService extends \Stripe\Service\AbstractService
      *
      * @param string $id
      * @param null|array $params
-     * @param null|array|\Stripe\Util\RequestOptions $opts
+     * @param null|RequestOptionsArray|\Stripe\Util\RequestOptions $opts
      *
      * @throws \Stripe\Exception\ApiErrorException if the request fails
      *
-     * @return \Stripe\Treasury\FinancialAccount
+     * @return \Stripe\Treasury\FinancialAccountFeatures
      */
     public function retrieveFeatures($id, $params = null, $opts = null)
     {
@@ -74,7 +78,7 @@ class FinancialAccountService extends \Stripe\Service\AbstractService
      *
      * @param string $id
      * @param null|array $params
-     * @param null|array|\Stripe\Util\RequestOptions $opts
+     * @param null|RequestOptionsArray|\Stripe\Util\RequestOptions $opts
      *
      * @throws \Stripe\Exception\ApiErrorException if the request fails
      *
@@ -90,11 +94,11 @@ class FinancialAccountService extends \Stripe\Service\AbstractService
      *
      * @param string $id
      * @param null|array $params
-     * @param null|array|\Stripe\Util\RequestOptions $opts
+     * @param null|RequestOptionsArray|\Stripe\Util\RequestOptions $opts
      *
      * @throws \Stripe\Exception\ApiErrorException if the request fails
      *
-     * @return \Stripe\Treasury\FinancialAccount
+     * @return \Stripe\Treasury\FinancialAccountFeatures
      */
     public function updateFeatures($id, $params = null, $opts = null)
     {
