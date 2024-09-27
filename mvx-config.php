d5b73d30255e38968bbe7514ac98b3e1428b4e49
Ujjwal Gorai <ujjwalgorai.29@gmail.com>
2024-09-27 15:30:41 +0530

    Changed xml files

--- mvx-config.php
+++ mvx-config.php
@@ -7,4 +7,4 @@ define('MVX_SCRIPT_DEBUG', false);
 
 define('MVX_PLUGIN_ASSEST', plugins_url( 'assets', __FILE__ ));
 
-define('MVX_PLUGIN_DUMMY_DATA', __DIR__.'\assets\dummy-data');
+define('MVX_PLUGIN_DUMMY_DATA', __DIR__.'/assets/dummy-data');
