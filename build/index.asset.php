df1c08edc418011b7372601bbd1775aeae9da3be
JoySarkar07 <joys18190@gmail.com>
2025-03-11 17:24:06 +0530

    restructure product_permissions setting page

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '9abc63b9ef01a07162ef');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '053283318b89b791d349');
